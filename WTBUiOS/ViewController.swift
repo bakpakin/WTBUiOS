e16e2e09ef6f0db799b2dc8768eb79643d02c1a9
bakpakin <calsrose@gmail.com>
2016-01-26 20:12:42 -0500

    Initial Commit

---
+++ WTBUiOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WTBUiOS
+//
+//  Created by Calvin Rose on 1/26/16.
+//  Copyright © 2016 Calvin Rose. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
